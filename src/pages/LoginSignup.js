af10322fe85af0a1a3e5017b9d5b05938c65d6cc
Pradipta <81964254+Pradipta22@users.noreply.github.com>
2023-02-08 12:45:42 +0530

    update

---
+++ src/pages/LoginSignup.js
@@ -0,0 +1,38 @@
+
+import Profile from "../components/Profile/Profile";
+import Login from "../components/Login/Login";
+import Register from "../components/Register/Register";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+
+function LogSign() {
+  const [userstate, setUserState] = useState({});
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              userstate && userstate._id ? (
+                <Profile
+                  setUserState={setUserState}
+                  username={userstate.fname}
+                />
+              ) : (
+                <Login setUserState={setUserState} />
+              )
+            }
+          ></Route>
+          <Route
+            path="/login"
+            element={<Login setUserState={setUserState} />}
+          ></Route>
+          <Route path="/signup" element={<Register />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default LogSign;
